dd6b2972b2c867b23c9582278db107c4960d9969
zivbabs <117026503+zivbabs@users.noreply.github.com>
2023-12-09 16:36:17 +0100

    change to new url

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ if (
 ) {
   API_URL = "http://localhost:8080/api";
 } else {
-  API_URL = "https://greenplate-backends.azurewebsites.net/api";
+  API_URL = "https://greenplatebackend.azurewebsites.net/api";
 }
 
 export { API_URL };
